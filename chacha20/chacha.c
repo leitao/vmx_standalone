e2e48c3ba9974a35af37e5e9a53ac0955ed14b5b
Paulo Flabiano Smorigo <pfsmorigo@gmail.com>
2017-10-03 19:27:23 +0000

    chacha20: clean unused code and use empty input for now

--- chacha20/chacha.c
+++ chacha20/chacha.c
@@ -50,46 +50,24 @@ int main(int argc, char **argv) {
 	uint32_t key[8] = {
 		0x03020100, 0x07060504, 0x0b0a0908, 0x0f0e0d0c,
 		0x13121110, 0x17161514, 0x1b1a1918, 0x1f1e1d1c};
-	uint32_t nonce[3] = {0x00, 0x4a000000, 0x0};
-	uint32_t counter = 0x1;
-	uint32_t input[16];
-	uint32_t output[16];
+	uint32_t counter[4] = {
+		0x00000001, 0x09000000, 0x4a000000, 0x00000000};
 	uint8_t plaintext[256];
 	uint8_t encrypted[256];
-	uint16_t text_size;
+	uint32_t text_size;
 
 	memset((char *) plaintext, 0, 256);
 	memset((char *) encrypted, 0, 256);
-	memset((uint8_t *) output, 0, 4*16);
-	sprintf((char *) plaintext, "Ladies and Gentlemen of the class of '99: If I could offer you only one tip for the future, sunscreen would be it.");
-	text_size = strlen((char *) plaintext);
 
-	/* sigma constant "expand 32-byte k" in little-endian encoding */
-	input[0] = ((uint32_t)'e') | ((uint32_t)'x'<<8) | ((uint32_t)'p'<<16) | ((uint32_t)'a'<<24);
-	input[1] = ((uint32_t)'n') | ((uint32_t)'d'<<8) | ((uint32_t)' '<<16) | ((uint32_t)'3'<<24);
-	input[2] = ((uint32_t)'2') | ((uint32_t)'-'<<8) | ((uint32_t)'b'<<16) | ((uint32_t)'y'<<24);
-	input[3] = ((uint32_t)'t') | ((uint32_t)'e'<<8) | ((uint32_t)' '<<16) | ((uint32_t)'k'<<24);
+	/*sprintf((char *) plaintext, "Ladies and Gentlemen of the class of '99: If I could offer you only one tip for the future, sunscreen would be it.");*/
+	/*text_size = strlen((char *) plaintext);*/
+	text_size = 64;
 
-	input[4] = key[0];
-	input[5] = key[1];
-	input[6] = key[2];
-	input[7] = key[3];
-	input[8] = key[4];
-	input[9] = key[5];
-	input[10] = key[6];
-	input[11] = key[7];
-
-	input[12] = counter;
-	input[13] = nonce[0];
-	input[14] = nonce[1];
-	input[15] = nonce[2];
-
-	hexprint32("input", input, 16);
 	hexprint("plaintext", plaintext, text_size);
-	ChaCha20_ctr32_vmx(encrypted, plaintext, text_size, key, &counter);
+	ChaCha20_ctr32_vmx(encrypted, plaintext, text_size, key, counter);
 	hexprint("encrypted", encrypted, text_size);
 	memset((char *) plaintext, 0, 256);
-	ChaCha20_ctr32_vmx(plaintext, encrypted, text_size, key, &counter);
+	ChaCha20_ctr32_vmx(plaintext, encrypted, text_size, key, counter);
 	hexprint("plaintext", plaintext, text_size);
 
 	return 0;
